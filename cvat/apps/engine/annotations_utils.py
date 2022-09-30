7f939a8a6deb18243e55635a7e3ad24cbe722cb4
sandesh sukubhattu <sandeshsukubhattu09@gmail.com>
2022-09-30 19:14:48 +0545

    Save all annotations in single request and payload

--- cvat/apps/engine/annotations_utils.py
+++ cvat/apps/engine/annotations_utils.py
@@ -69,3 +69,46 @@ def save_annotations_to_polygon_backup(data, headers):
         }
         requests.post(f'http://ec2co-ecsel-120oaoc0msxmg-363566620.us-east-1.elb.amazonaws.com:8081/images/user/{user_id}/org/{org.id}/task/{task.id}/image/{frame}/saveannotationfromcvat', headers=headers, json=json_data)
 
+def save_annotations_to_polygon(data, headers):
+    tracks = data["tracks"]
+    shapes = data["shapes"]
+    annotations_list = []
+
+    for track in tracks:
+        annotation_dict = {}
+        label_id = track['label_id']
+        label = Label.objects.get(id = label_id)
+        task = Task.objects.get(label__id = label_id)
+        user_id = task.owner_id
+        org_id = task.organization_id
+        org = Organization.objects.get(id = org_id)
+        frame = track['shapes'][0]['frame']
+        points = track['shapes'][0]['points']
+        annotationPoints = generate_annotation_points(points)
+        annotation_dict["imageId"] = str(frame)
+        annotation_dict["orgId"] = str(org.id)
+        annotation_dict["taskId"] = str(task.id)
+        annotation_dict["objectName"] = label.name
+        annotation_dict["annotationPolygonList"] = generate_annotation_points(annotationPoints)
+        annotations_list.append(annotation_dict)
+
+    for shape in shapes:
+        annotation_dict = {}
+        label_id = track['label_id']
+        label = Label.objects.get(id = label_id)
+        task = Task.objects.get(label__id = label_id)
+        user_id = task.owner_id
+        org_id = task.organization_id
+        org = Organization.objects.get(id = org_id)
+        frame = shape['frame']
+        points = shape['points']
+        annotationPoints = generate_annotation_points(points)
+        annotation_dict["imageId"] = str(frame)
+        annotation_dict["orgId"] = str(org.id)
+        annotation_dict["taskId"] = str(task.id)
+        annotation_dict["objectName"] = label.name
+        annotation_dict["annotationPolygonList"] = generate_annotation_points(annotationPoints)
+        annotations_list.append(annotation_dict)
+
+    json_data = {'annotationList': annotations_list}
+    requests.post(f'http://ec2co-ecsel-120oaoc0msxmg-363566620.us-east-1.elb.amazonaws.com:8081/images/user/{user_id}/org/{org.id}/task/{task.id}/image/{frame}/saveannotationfromcvat', headers=headers, json=json_data)
